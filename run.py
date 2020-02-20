665254a11bb0a2fa799a3dd0cabf2eb7c9a387eb
LuosRestil <metaltasteslikechicken@gmail.com>
2020-02-19 21:14:38 -0600

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+from flaskblog import app
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+
+if __name__ == '__main__':
+    app.run()
