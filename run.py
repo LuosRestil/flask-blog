de06ee11761b1909a0551a8c7d799185fc6adfc5
LuosRestil <metaltasteslikechicken@gmail.com>
2020-02-20 13:50:42 -0600

    Added custom error pages and error handler blueprint

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ from flaskblog import create_app
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
 from flaskblog.main.routes import main
+from flaskblog.errors.handlers import errors
 
 app = create_app()
 
 app.register_blueprint(users)
 app.register_blueprint(posts)
 app.register_blueprint(main)
+app.register_blueprint(errors)
 
 if __name__ == '__main__':
     app.run()
